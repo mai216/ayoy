e9760699ca87fe1633ea7f2d78aae537aa38e654
rahmaAmer1000 <rahmaAmer1000@gmail.com>
2024-02-14 16:57:21 +0200

    jj

--- ayoy/src/App.js
+++ ayoy/src/App.js
@@ -1,5 +1,18 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+import { FaHeart } from 'react-icons/fa';
+import img from '../src/14005cf3-7b44-4214-99f6-8ce5cc3dd744.jpeg';
+
+
+ 
+ 
+
+
+
+
+
+
+
 
 const Container = styled.div`
   display: flex;
@@ -9,6 +22,9 @@ const Container = styled.div`
 `;
 
 const Button = styled.button`
+
+
+
   padding: 10px 20px;
   background-color: #ff4081;
   color: #fff;
@@ -20,25 +36,49 @@ const Button = styled.button`
 `;
 
 const Card = styled.div`
-  width: 400px;
-  height: 300px;
-  background: linear-gradient(to right, #ff4081, #ff8f73);
-  border-radius: 20px;
-  padding: 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 300px;
+  height: 400px;
+  background-color: #fff;
+  border-radius: 15px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.15);
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
-  color: #fff;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.15);
+  align-items: center;
+  justify-content: center;
+  opacity: ${({ show }) => (show ? 1 : 0)};
+  transition: opacity 0.5s ease;
 `;
 
-const CardTitle = styled.h2`
-  font-size: 24px;
-  margin-top: 0;
+const CardImage = styled.img`
+  width: 200px;
+  height: auto;
+  margin-bottom: 20px;
 `;
 
 const CardMessage = styled.p`
   font-size: 18px;
+`;
+
+const HeartAnimation = keyframes`
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+`;
+
+const HeartIcon = styled(FaHeart)`
+  font-size: 48px;
+  color: #ff4081;
+  animation: ${HeartAnimation} 1s infinite;
 `;
 
 const App = () => {
@@ -50,13 +90,12 @@ const App = () => {
 
   return (
     <Container>
-      {!showCard && <Button onClick={handleButtonClick}>Open Card</Button>}
+      <Button onClick={handleButtonClick}>دوس هنا</Button>
       {showCard && (
-        <Card>
-          <CardTitle>Happy Valentine's Day!</CardTitle>
-          <CardMessage>
-            Wishing you a day filled with love, joy, and happiness.
-          </CardMessage>
+        <Card show={showCard}>
+          <CardImage src={img} alt="Valentine's Day Card" />
+          <CardMessage>Happy Valentine's Day!</CardMessage>
+          <HeartIcon />
         </Card>
       )}
     </Container>
